63c8789084cbd42b4975e52dcc9df90917466db6
Daniel Conte <daniel.conte@meumobi.com>
2018-06-08 15:44:41 -0300

    refactor: move pipe to shared module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { MeuToastProvider } from './meu-toast.service';
 import { AnalyticsProvider } from './analytics.service';
+import { ImagecdnPipe } from '../pipes/imagecdn/imagecdn';
 
 
 @NgModule({
   imports: [],
   declarations: [
+    ImagecdnPipe
   ],
   providers: [
     MeuToastProvider,
     AnalyticsProvider
   ],
   exports: [
+    ImagecdnPipe
   ]
 })
 export class SharedModule { }
